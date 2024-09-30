dd1b4fa5cb6522a50df8ff89d49bba9ab99e30a3
it-is-zane <zane.gant@gmail.com>
2024-09-30 13:14:23 -0700

    Updated main.rs
    Removed the flux stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use rand::seq::index;
-use serde::{Deserialize, Serialize};
+use ratatui::layout;
 
-#[derive(Deserialize, Serialize, Debug)]
+#[allow(non_camel_case_types)]
+#[derive(serde::Deserialize, serde::Serialize, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum UsageCategory {
+    core,
+    common,
+    uncommon,
+    obscure,
+    sandbox,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
 pub struct WordData {
     pub id: String,
-    pub usage_category: String,
+    pub usage_category: UsageCategory,
     pub word: String,
     pub deprecated: bool,
     pub ku_data: Option<std::collections::HashMap<String, u16>>,
@@ -14,7 +23,7 @@ pub struct WordData {
 }
 
 static WORDS: std::sync::LazyLock<Vec<WordData>> = std::sync::LazyLock::new(|| {
-    #[derive(Deserialize, Serialize, Debug)]
+    #[derive(serde::Deserialize, serde::Serialize, Debug)]
     struct Words {
         words: Vec<WordData>,
     }
@@ -38,213 +47,283 @@ static WORDS: std::sync::LazyLock<Vec<WordData>> = std::sync::LazyLock::new(|| {
     }
 });
 
-#[derive(Debug)]
-enum Action {
-    Char(char),
-    Backspace,
-    Goto(Page),
-    ApplyGameSettigns(GameSettings),
+enum TextRenderType {
+    Correct(String),
+    Incorrect { target: String, input: String },
+    Excess(String),
+    NoInput(String),
+}
+
+struct Word<'a> {
+    data: &'a WordData,
+    time: std::time::Duration,
+    errors: usize,
+}
+
+struct Game<'a> {
+    words: Vec<Word<'a>>,
+    input: String,
+    time: std::time::Duration,
+}
+
+#[derive(Default, Clone, Copy)]
+struct GameSettings {
+    in_use: bool,
+    deprecated: bool,
+    core: bool,
+    common: bool,
+    uncommon: bool,
+    obscure: bool,
+    sandbox: bool,
+    ku: bool,
+    pu: bool,
+    commentary: bool,
+    definitions: bool,
+    n: usize,
+}
+
+impl Game<'_> {
+    fn new(settings: GameSettings) -> Self {
+        use rand::seq::SliceRandom;
+
+        let word_iter = WORDS
+            .iter()
+            .filter(|data| settings.in_use | data.deprecated)
+            .filter(|data| settings.deprecated | !data.deprecated)
+            .filter(|data| settings.core | (data.usage_category != UsageCategory::core))
+            .filter(|data| settings.common | (data.usage_category != UsageCategory::common))
+            .filter(|data| settings.uncommon | (data.usage_category != UsageCategory::uncommon))
+            .filter(|data| settings.obscure | (data.usage_category != UsageCategory::obscure))
+            .filter(|data| settings.sandbox | (data.usage_category != UsageCategory::sandbox))
+            .filter(|data| settings.ku | data.ku_data.is_some())
+            .filter(|data| settings.pu | data.pu_verbatim.is_some())
+            .filter(|data| settings.commentary | data.commentary.is_some())
+            .filter(|data| settings.definitions | data.definitions.is_some());
+
+        let mut words: Vec<Word> = word_iter
+            .map(|data| Word {
+                data,
+                time: std::time::Duration::default(),
+                errors: 0,
+            })
+            .collect();
+
+        words.drain((settings.n)..);
+
+        words.shuffle(&mut rand::thread_rng());
+
+        Game {
+            words,
+            input: String::new(),
+            time: std::time::Duration::default(),
+        }
+    }
+    fn render(&self) -> Vec<TextRenderType> {
+        use TextRenderType::*;
+        self.words
+            .iter()
+            .map(|word| &word.data.word)
+            .map(Some)
+            .zip(
+                self.input
+                    .split(' ')
+                    .map(Some)
+                    .chain(std::iter::once(None).cycle()),
+            )
+            .fold(Vec::new(), |mut acc, vals| {
+                match vals {
+                    (Some(target), None) => {
+                        if let Some(NoInput(str)) = acc.last_mut() {
+                            str.push_str(target);
+                        } else {
+                            acc.push(NoInput(target.clone()));
+                        }
+                    }
+                    (Some(target), Some(input)) => {
+                        target
+                            .chars()
+                            .map(Some)
+                            .chain(std::iter::once(None).cycle())
+                            .zip(input.chars().map(Some).chain(std::iter::once(None).cycle()))
+                            .take(target.len().max(input.len()))
+                            .for_each(|char_pair| match char_pair {
+                                (None, None) => (),
+                                (None, Some(c)) => {
+                                    if let Some(Excess(str)) = acc.last_mut() {
+                                        str.push(c);
+                                    } else {
+                                        acc.push(Excess(c.to_string()));
+                                    }
+                                }
+                                (Some(c), None) => {
+                                    if let Some(NoInput(str)) = acc.last_mut() {
+                                        str.push(c);
+                                    } else {
+                                        acc.push(NoInput(c.to_string()));
+                                    }
+                                }
+                                (Some(t), Some(c)) if t == c => {
+                                    if let Some(Correct(str)) = acc.last_mut() {
+                                        str.push(c);
+                                    } else {
+                                        acc.push(Correct(c.to_string()));
+                                    }
+                                }
+                                (Some(t), Some(c)) => {
+                                    if let Some(Incorrect { target, input }) = acc.last_mut() {
+                                        target.push(t);
+                                        input.push(c);
+                                    } else {
+                                        acc.push(Incorrect {
+                                            target: t.to_string(),
+                                            input: c.to_string(),
+                                        });
+                                    }
+                                }
+                            });
+                    }
+                    _ => (),
+                };
+
+                if let Some(item) = acc.last_mut() {
+                    match item {
+                        Correct(str) => str.push(' '),
+                        Incorrect {
+                            target: _,
+                            input: _,
+                        }
+                        | Excess(_)
+                        | NoInput(_) => acc.push(Correct(" ".to_string())),
+                    }
+                };
+
+                acc
+            })
+    }
+}
+
+enum Page {}
+enum State {
+    Game { settings: GameSettings },
+    Results {},
+    Settings,
     Exit,
 }
 
-#[derive(Default)]
-struct Dispatcher {
-    stores: Vec<std::rc::Rc<std::cell::RefCell<dyn Store>>>,
-    queue: std::collections::VecDeque<Action>,
-}
-
-impl Dispatcher {
-    fn new() -> Self {
-        Dispatcher::default()
-    }
-    fn register<T: Store + 'static>(&mut self, store: T) -> std::rc::Rc<std::cell::RefCell<T>> {
-        let rc = std::rc::Rc::new(std::cell::RefCell::new(store));
-        self.stores.push(rc.clone());
-        rc
-    }
-    fn action(&mut self, action: Action) {
-        self.queue.push_back(action);
-    }
-    fn update(&mut self) {
-        let mut new_queue = std::collections::VecDeque::new();
-
-        self.queue.iter().for_each(|action| {
-            self.stores
-                .iter_mut()
-                .for_each(|store| store.borrow_mut().update(action, &mut new_queue))
-        });
-
-        self.queue = new_queue;
-    }
-}
-
-trait Store {
-    fn update(&mut self, action: &Action, queue: &mut std::collections::VecDeque<Action>);
-}
-
-#[derive(Copy, Clone, Debug)]
-enum Page {
-    Game,
-    Results,
-}
-
-impl Store for Page {
-    fn update(&mut self, action: &Action, queue: &mut std::collections::VecDeque<Action>) {
-        if let Action::Goto(page) = action {
-            *self = *page;
+fn get_char(event: &ratatui::crossterm::event::Event) -> Option<char> {
+    if let ratatui::crossterm::event::Event::Key(key) = event {
+        if let ratatui::crossterm::event::KeyCode::Char(c) = key.code {
+            return Some(c);
         }
     }
-}
 
-impl Store for bool {
-    fn update(&mut self, action: &Action, queue: &mut std::collections::VecDeque<Action>) {
-        if let Action::Exit = action {
-            *self = true
-        }
-    }
-}
-
-struct Word {
-    target: String,
-    input: String,
-    info: String,
-    enter: Option<std::time::Instant>,
-    duration: std::time::Duration,
-}
-
-impl Word {
-    fn new(target: String, info: String) -> Self {
-        Self {
-            target,
-            input: String::new(),
-            info,
-            enter: None,
-            duration: std::time::Duration::default(),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct GameSettings {
-    word_count: usize,
-}
-
-#[derive(Default)]
-struct Game {
-    words: Vec<Word>,
-    index: usize,
-    start: Option<std::time::Instant>,
-}
-
-impl Store for Game {
-    fn update(&mut self, action: &Action, queue: &mut std::collections::VecDeque<Action>) {
-        match action {
-            Action::ApplyGameSettigns(settings) => {
-                self.words = WORDS
-                    .iter()
-                    .filter_map(|word| {
-                        word.definitions
-                            .clone()
-                            .map(|def| Word::new(word.word.clone(), def))
-                    })
-                    .take(settings.word_count)
-                    .collect();
-
-                use rand::seq::SliceRandom;
-                self.words.shuffle(&mut rand::thread_rng());
-            }
-            Action::Char(' ') => {
-                if let Some(word) = self.words.get_mut(self.index) {
-                    if let Some(enter) = word.enter {
-                        word.duration += enter.elapsed();
-                    }
-
-                    word.enter = None;
-                }
-
-                self.index += 1;
-
-                if self.index >= self.words.len() - 1 {
-                    queue.push_back(Action::Goto(Page::Results));
-                }
-            }
-            Action::Backspace => {
-                if let Some(i) = self.index.checked_sub(1) {
-                    if let Some(word) = self.words.get_mut(self.index) {
-                        if let Some(enter) = word.enter {
-                            word.duration += enter.elapsed();
-                        }
-
-                        word.enter = None;
-                    }
-
-                    self.index = i;
-                }
-            }
-            Action::Char(c) => {
-                if let Some(word) = self.words.get_mut(self.index) {
-                    word.input.push(*c);
-
-                    if word.enter.is_none() {
-                        word.enter = Some(std::time::Instant::now());
-                    }
-                }
-            }
-            _ => (),
-        }
-    }
+    None
 }
 
 fn main() {
     let mut terminal = ratatui::init();
-    terminal.clear().unwrap();
 
-    let mut dis = Dispatcher::new();
-
-    let should_exit = dis.register(false);
-    let page = dis.register(Page::Game);
-    let game = dis.register(Game::default());
-
-    dis.action(Action::ApplyGameSettigns(GameSettings { word_count: 10 }));
+    let mut state = State::Settings;
 
     loop {
-        dis.update();
+        match state {
+            State::Game { settings } => {
+                let mut game = Game::new(settings);
 
-        if *should_exit.borrow_mut() {
-            break;
-        }
+                let layout = ratatui::layout::Layout::vertical([
+                    ratatui::layout::Constraint::Min(4),
+                    ratatui::layout::Constraint::Fill(1),
+                    ratatui::layout::Constraint::Length(1),
+                ]);
 
-        let game = game.borrow();
-        let index = game.index;
+                let padding =
+                    ratatui::widgets::Block::new().padding(ratatui::widgets::Padding::uniform(1));
 
-        _ = match *page.borrow_mut() {
-            Page::Game => terminal.draw(|_frame| {
-                _frame.render_widget(
-                    game.words
-                        .iter()
-                        .map(|word| word.target.clone() + " ")
-                        .collect::<String>()
-                        + &index.to_string()
-                        + " "
-                        + &game.words[index].target,
-                    _frame.area(),
-                )
-            }), // draw game
-            Page::Results => terminal.draw(|_frame| _frame.render_widget("results", _frame.area())), // draw results
-        };
+                loop {
+                    use ratatui::style::Stylize;
+                    terminal
+                        .draw(|frame| {
+                            frame.render_widget(&padding, layout.split(frame.area())[0]);
 
-        if let Ok(ratatui::crossterm::event::Event::Key(key)) = ratatui::crossterm::event::read() {
-            let ctrl = key
-                .modifiers
-                .contains(ratatui::crossterm::event::KeyModifiers::CONTROL);
+                            frame.render_widget(
+                                ratatui::widgets::Block::new()
+                                    .title("white")
+                                    .title_alignment(ratatui::layout::Alignment::Center)
+                                    .on_white()
+                                    .black(),
+                                padding.inner(layout.split(frame.area())[0]),
+                            );
 
-            use ratatui::crossterm::event::KeyCode::*;
-            match key.code {
-                Backspace => dis.action(Action::Backspace),
-                Char('c') if ctrl => dis.action(Action::Exit),
-                Char(c) => dis.action(Action::Char(c)),
-                _ => (),
+                            frame.render_widget(&padding, layout.split(frame.area())[1]);
+
+                            use ratatui::text::ToSpan;
+                            frame.render_widget(
+                                ratatui::widgets::Paragraph::new(
+                                    game.render()
+                                        .iter()
+                                        .map(|item| -> ratatui::prelude::Span {
+                                            match item {
+                                                TextRenderType::Correct(str) => {
+                                                    str.to_span().white()
+                                                }
+                                                TextRenderType::Incorrect { target, input: _ } => {
+                                                    target.to_span().red().underlined()
+                                                }
+                                                TextRenderType::Excess(str) => {
+                                                    str.to_span().yellow()
+                                                }
+                                                TextRenderType::NoInput(str) => {
+                                                    str.to_span().dark_gray()
+                                                }
+                                            }
+                                        })
+                                        .collect::<ratatui::text::Line>(),
+                                )
+                                .wrap(ratatui::widgets::Wrap { trim: false }),
+                                padding.inner(layout.split(frame.area())[1]),
+                            );
+
+                            frame.render_widget(
+                                ratatui::widgets::Block::new()
+                                    .title("Black")
+                                    .title_alignment(ratatui::layout::Alignment::Center),
+                                layout.split(frame.area())[2],
+                            );
+                        })
+                        .unwrap();
+
+                    let event = ratatui::crossterm::event::read().unwrap();
+
+                    if let Some('q') = get_char(&event) {
+                        break;
+                    }
+
+                    if let Some(c) = get_char(&event) {
+                        game.input.push(c);
+                    }
+
+                    if let ratatui::crossterm::event::Event::Key(event) = event {
+                        if let ratatui::crossterm::event::KeyCode::Backspace = event.code {
+                            _ = game.input.pop();
+                        }
+                    }
+                }
+
+                state = State::Results {};
             }
+            State::Results {} => todo!(),
+            State::Settings => {
+                state = State::Game {
+                    settings: GameSettings {
+                        n: 100,
+                        in_use: true,
+                        definitions: true,
+                        core: true,
+                        ..Default::default()
+                    },
+                };
+            }
+            State::Exit => break,
         }
     }
 
